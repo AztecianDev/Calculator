b65110e5b6d9cad79d945becf59a46b2425b9f98
Az <aztecian1122@gmail.com>
2019-07-03 14:28:19 +0100

    p tag added to html, onclick added to js

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
-const result = document.getElementById("#result");
+const result = document.getElementById("result , p");
 const choices = document.querySelectorAll(".choice");
 
 
 // add function
-const add = function (a, b) {
+function add(a, b) {
     return a + b;
 }
 
 //minus function
-const minus = function (a, b) {
+function minus(a, b) {
     return a - b;
 }
 
 
 //divide function
-const divide = function (a, b) {
+function divide(a, b) {
     return a / b;
 }
 
 // times function
-const multiply = function (a, b) {
+function multiply(a, b) {
     return a * b;
 }
 
 
-// sum function
+// operate function
+function operate(num1, num2, operators) {
 
-let operate = function (num1, num2, callback, ) {
-
-    return callback(num1, num2);
+    return operators(num1, num2);
 
 }
+// console.log(operate(6, 6, add));
+function display(e) {
+    const input = e.target.id;
+    console.log(input);
+}
 
-// console.log(operate(6, 6, multiply));
 
-choices.forEach(choice => {
-    choice.addEventListener('click', (e) => {
-        console.log(e.target.id);
-    });
-});
+choices.forEach(choice => choice.addEventListener('click', display));
