dedffb938d021b4ed807b1e162d5c02e6ce076a5
Aaron <aztcian1122@gmail.com>
2019-07-07 17:10:39 +0100

    added if statement to operate function

--- main.js
+++ main.js
@@ -24,30 +24,31 @@ function multiply(a, b) {
 
 // operate function
 // takes in operator numbers and operator name (function name).
-function operate(num1, num2, operators) {
-  return operators(num1, num2);
+function operate(operator, num1, num2) {
+  if(operator === "+"){
+    return add(a,b);
+  }else if(operator === "-"){
+    return minus(a,b)
+  }else if(operator === "*"){
+    return multiply(a,b);
+  }else if (operator === "/"){
+    return divide(a,b);
+  }
 }
-// console.log(operate(6, 6, add));
 
 // display numbers clicked to display.
-function numbersHandler(e) {
-  const number = e.target.id; // id of button clicked stored in variable number.
-  display.textContent = number;
-
-  console.log(number);
-}
-
-// display operators clicked to display.
-function operatorsHandler(e) {
-  const output = e.target.id;
-  display.textContent = output;
-
-  console.log(output);
-}
-
-//  eventlistner for numbers clicked.
-btnNumbers.forEach(btnNum => btnNum.addEventListener("click", numbersHandler));
+btnNumbers.forEach((btnNum) => {
+  btnNum.addEventListener('click',(e) =>{
+    let clickedNum = e.target.id;
+    displayVal = clickedNum;
+    display.textContent = displayVal;
+      
+  });
+});
 // eventlistner for operators.
-btnOperators.forEach(btnOp =>
-  btnOp.addEventListener("click", operatorsHandler)
-);
+btnOperators.forEach((btnOp) => {
+  btnOp.addEventListener('click',(e) =>{
+    const clickedOp = e.target.id;
+    display.textContent = clickedOp;
+  });
+});
